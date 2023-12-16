86d52b88f7ae70069c6df55db1a6d9f9fb13d5a8
J0SePh44 <150965391+J0SePh44@users.noreply.github.com>
2023-12-16 14:06:05 -0500

    juego_cartas.java

--- juego_cartas.java
+++ juego_cartas.java
@@ -9,7 +9,13 @@ public class cartas {
 		
 		Random rand = new Random();
 	    Scanner scanner = new Scanner(System.in); 
-	    
+	    do {
+			
+			System.out.println("OPRIMA ENTER PARA REPETIR");
+			String input = scanner.nextLine();
+		
+			if (input.isEmpty()) { 
+				
 	    int dado1 = rand.nextInt(13) + 1;
 	    int dados2 = rand.nextInt(10) + 1; 
 	    int total;
@@ -17,10 +23,8 @@ public class cartas {
 		int J=10;
 		int Q=10;
 		int K=10;
-	   
-	    
+		 
 	    System.out.println("EL RESULTADO DE LA PRIMERA CARTA ES:" + dado1);
-	 
 	    
 	   switch (dado1) {
 	   	case 11: { 
@@ -42,42 +46,63 @@ public class cartas {
 	   }
 	
 	   System.out.println("EL RESULTADO DE LA SEGUNDA CARTA ES:" + dados2);
-	   
-	   
+	  
+
 	   total= dado1+dados2;
 	switch (total) {
-	case 12: {
-		total=12;
-			System.out.println("repetir con 12");
-		
-	}
+	case 12:
+	if(dado1 == 11 && dados2 == 1) {
+        System.out.println("GANÓ CON 11"); 
+    } else if (total == 12) {
+        System.out.println("Repetir con 12");
+    }
+	
 	break;
-	case 11: {
-		if (dado1==J && dados2==1) {
-			total=11;
+	case 13: {
+		
+		if (dado1==12 && dados2==1) {
 			System.out.println("GANÓ CON 11");
-		}
-		if (dado1==Q && dados2==1) {
-			total=11;
-			System.out.println("GANÓ CON 11");
-		}
-		if (dado1==K && dados2==1) {
-			total=11;
-			System.out.println("GANÓ CON 11");
-		}
+			System.out.println("GANAS ESTA PARTIDA");
+		}	
 	}
 	break;
 	
 	case 14: {
-		total=14;
-		System.out.println("medio ganaste");
+		if (dado1==13 && dados2==1) {
+			System.out.println("GANÓ CON 11");
+			System.out.println("GANAS ESTA PARTIDA");
+		} else if (total==14) {
+			System.out.println("medio ganaste");
+		}
 	}
+	break;
+	
+	case 2: {
+		total=2;
+		System.out.println("GANAS ESTA PARTIDA");
+	}
+	break;
 	
 	case 21: { 
-		if (total>21) {
+		if (total==21) {
+			System.out.println("HAS OBTENIDO UNA SUMATORIA IGUAL A 21, SI OBTIENES UNA MAYOR PIERDES");
+		}
+
+		else if (total>21) {
 			System.out.println("HAS OBTENIDO UNA SUMATORIA MAYOR A 21, PIERDES");
 		}
 	}
+	break;
+			
 	}// del cierre del switch
-	}//del public
-}
+	
+			}
+		 else if (input.equalsIgnoreCase("9")) {
+	        System.out.println("JUEGO FINALIZADO.");
+	        break; 
+	    } 
+		
+		} while (true);
+		 
+	   }//del public
+     }
